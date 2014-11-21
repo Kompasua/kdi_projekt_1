e824108e635148bf4610a5413bd5cc7ae12b27ad
Anton <uaantony@gmail.com>
2014-11-21 22:35:14 +0100

    Next step method

--- src/Ghost.java
+++ src/Ghost.java
@@ -1,9 +1,11 @@
 // Ghost.java
 // Used for PaKman
 
-import ch.aplu.jgamegrid.*;
 import java.awt.Color;
-import java.util.*;
+import java.awt.Point;
+
+import ch.aplu.jgamegrid.Actor;
+import ch.aplu.jgamegrid.Location;
 
 public class Ghost extends Actor
 {
@@ -15,7 +17,6 @@ public class Ghost extends Actor
         this.pakman = pakman;
         reset();
     }
-
     
     /**
      * Called when the level is initialized or reset.
@@ -24,22 +25,37 @@ public class Ghost extends Actor
 
     }
     
-    
     /**
      * Called once in every iteration of the game loop to calculate the actions
      * of this ghost.
      */
     public void act()
-    {
-    
-        
-        // When moving westwards, mirror the sprite so it looks in the proper direction
+    {	 
+    	nextStep();
+    	// When moving westwards, mirror the sprite so it looks in the proper direction
         if (getDirection() > 150 && getDirection() < 210)
             setHorzMirror(false);
         else
             setHorzMirror(true);
+        if (!isMoveValid())
+            turn(180);
+        if (isInGrid());
+        	//move();
+    }
+    private boolean canMove(Location location)
+    {
+      Color c = getBackground().getColor(location);
+      return (!c.equals(Color.gray));
     }
     
+    private void nextStep(){
+    	Location next = getLocation().getNeighbourLocation(Location.EAST);
+    	if (next != null && canMove(next))
+        {
+          setLocation(next);
+          gameGrid.refresh();
+        }
+    }
     
     /**
      * Toggle hunting/fleeing mode.
