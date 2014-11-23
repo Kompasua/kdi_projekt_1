d1d813dc57bdc143ccb7767c35c7bc89930bd286
Anton <uaantony@gmail.com>
2014-11-23 20:07:04 +0100

    Add moving logic prototype

--- src/Ghost.java
+++ src/Ghost.java
@@ -3,6 +3,8 @@
 
 import java.awt.Color;
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Random;
 
 import ch.aplu.jgamegrid.Actor;
 import ch.aplu.jgamegrid.Location;
@@ -10,6 +12,8 @@ import ch.aplu.jgamegrid.Location;
 public class Ghost extends Actor
 {
     private PaKman pakman;
+    private Location last;
+    boolean first = true;
 
     public Ghost(PaKman pakman)
     {
@@ -31,30 +35,121 @@ public class Ghost extends Actor
      */
     public void act()
     {	 
-    	nextStep();
+    	
+    	if (first == true){
+    		goRandom();
+    		first = false;
+    	}else{
+    		nextStep();
+    	}
     	// When moving westwards, mirror the sprite so it looks in the proper direction
         if (getDirection() > 150 && getDirection() < 210)
             setHorzMirror(false);
         else
             setHorzMirror(true);
+        //If end of the map and move is impossible, then turn over
         if (!isMoveValid())
             turn(180);
-        if (isInGrid());
-        	//move();
     }
+    /**
+     * Check by color if cell available for move of ghost. Ghost is moving only on lightGray cells.   
+     * @param location where ghost would like to move
+     * @return true if sell is lightGray, false if not
+     */
     private boolean canMove(Location location)
     {
-      Color c = getBackground().getColor(location);
-      return (!c.equals(Color.gray));
+    	Color c = getBackground().getColor(location);
+    	return (c.equals(Color.lightGray));
     }
     
     private void nextStep(){
-    	Location next = getLocation().getNeighbourLocation(Location.EAST);
+    	System.out.println("NO random");
+    	Location east = getLocation().getNeighbourLocation(Location.EAST);
+    	Location west = getLocation().getNeighbourLocation(Location.WEST);
+    	Location north = getLocation().getNeighbourLocation(Location.NORTH);
+    	Location south = getLocation().getNeighbourLocation(Location.SOUTH);
+    	Location next = getLocation().getNeighbourLocation(last.getDirectionTo(getLocation()));
+    	System.out.println("Last: "+last.toString());
+    	System.out.println("Now: "+getLocation());
+    	System.out.println("Next: "+next.toString());
     	if (next != null && canMove(next))
         {
-          setLocation(next);
-          gameGrid.refresh();
+    		if (lookAround().size() > 2){
+    			System.out.println(lookAround().toString());
+    			goRandom();
+    		}else{
+    			setLocation(next);
+    	        gameGrid.refresh();
+    		}
+        }else{
+        	goRandom();
         }
+    	//setLocation(east);
+    	//System.out.println(last.toString());
+    	//System.out.println(next.toString());
+    	//System.out.println(getLocation());
+    	/*System.out.println((int)last.getDirectionTo(getLocation()));
+    	switch((int)last.getDirectionTo(getLocation())){
+    	case 0:
+    		if (east != null && canMove(east))
+            {
+    			last = getLocation();
+              setLocation(east);
+              gameGrid.refresh();
+            }else{
+            	goRandom();
+            }
+    	case 90:
+    		if (south != null && canMove(south))
+            {
+              setLocation(south);
+              gameGrid.refresh();
+            }else{
+            	goRandom();
+            }
+    	case 180:
+    		if (west != null && canMove(west))
+            {
+    			last = getLocation();
+              setLocation(west);
+              gameGrid.refresh();
+            }else{
+            	goRandom();
+            }
+    		
+    	}*/
+    	/*if (east != null && canMove(east))
+        {
+          setLocation(east);
+          gameGrid.refresh();
+        }*/
+    	
+    }
+    private ArrayList lookAround(){
+    	Location east = getLocation().getNeighbourLocation(Location.EAST);
+    	Location west = getLocation().getNeighbourLocation(Location.WEST);
+    	Location north = getLocation().getNeighbourLocation(Location.NORTH);
+    	Location south = getLocation().getNeighbourLocation(Location.SOUTH);
+    	
+    	ArrayList<Location> directions = new ArrayList<Location>();
+    	if (canMove(east))
+    		directions.add(east);
+    	if (canMove(west))
+    		directions.add(west);
+    	if (canMove(north))
+    		directions.add(north);
+    	if (canMove(south))
+    		directions.add(south);
+    	return directions;
+    }
+    private void goRandom(){
+    	System.out.println("Random");
+    	
+    	Random generator = new Random(); 
+    	int i = generator.nextInt(lookAround().size());
+    	setLocation((Location) lookAround().get(i));
+    	last = getLocation();
+    	gameGrid.refresh();
     }
     
     /**
