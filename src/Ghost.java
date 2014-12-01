47bb2f4d4986e103b108ac2d672cf8bfe51ab20f
Anton <uaantony@gmail.com>
2014-12-01 14:58:07 +0100

    Comments

--- src/Ghost.java
+++ src/Ghost.java
@@ -65,8 +65,15 @@ public class Ghost extends Actor {
 		}
 		return false;
 	}
-
+	
+	/**
+	 * If possible and if we must'nt make random step,
+	 * then make step in near location (by direction).
+	 * If move in this direction is impossible then call goRandom() method.
+	 * @param _direction for next step of ghost
+	 */
 	private void makeStep(double _direction) {
+		//Location where ghost would like to move.
 		Location next = getLocation().getNeighbourLocation(dirStabilizer(_direction));
 		if (canMove(dirStabilizer(_direction)) && random == 0) {
 			last = getLocation();
@@ -78,7 +85,14 @@ public class Ghost extends Actor {
 			goRandom();
 		}
 	}
-
+	
+	/**
+	 * Make next depending on pakman location (east,west,north, south). 
+	 * If pakman location is south-west, east-west and so on, then make
+	 * step in the same direction. 
+	 * If no one of described variants is possible, 
+	 * then make one random step.
+	 */
 	private void nextStep() {
 		//Direction to pakman location in this step.
 		double pakmanLoc = getLocation().getDirectionTo(pakman.wherePakman()); 
@@ -104,12 +118,22 @@ public class Ghost extends Actor {
 			makeStep(direction);
 			return;
 		}
+		//Set only one random step if we're not in 10 
+		// random steps mode.
 		if (random == 0){
 			random = 1;
 		}
 		goRandom();
 	}
-
+	
+	/**
+	 * Stabilize direction of movement for Location class methods. 
+	 * Check if direction in range from 0 to 359.
+	 * When not, then change it so to be in this range.
+	 * Else return same direction.
+	 * @param direction of pakman movement (not stabilized)
+	 * @return direction of pakman movement (stabilized)
+	 */
 	private double dirStabilizer(double direction) {
 		if (direction < 0)
 			return 360 - direction * (-1);
@@ -120,6 +144,13 @@ public class Ghost extends Actor {
 		return direction;
 	}
 
+	/**
+	 * Collect possible directions for move.
+	 * First collect directions in straight, right and left
+	 * directions. If move in those directions is not possible
+	 * then turn over (go back).
+	 * @return Array with possible directions
+	 */
 	private ArrayList<Double> getNextSteps() {
 		ArrayList<Double> directions = new ArrayList<Double>();
 		if (canMove(direction)) {
@@ -138,17 +169,31 @@ public class Ghost extends Actor {
 		return directions;
 	}
 
+	/**
+	 * Make random step in available directions (get them from
+	 * lookAround method).
+	 * Direction of step depends from random.
+	 * If we have 3 possible directions, then choose 
+	 * one of them with chance 25% for each of right or 
+	 * left turn and 50% for straight move.
+	 * Else go in available two or one direction with 
+	 * chance 50% or 100% respectively. 
+	 */
 	private void goRandom() {
+		//If we're not in random mode, then "activate" it. 
 		if (random == 0) {
 			random = 10;
 		} else {
 			random--;
+			//Get available for move directions.
 			ArrayList<Double> directions = getNextSteps();
+			//Store possible directions number.
 			int variants = directions.size();
 			Random generator = new Random();
 			int i = generator.nextInt(variants);
-			last = getLocation();
+			last = getLocation(); //Save current location.
 
+			//Choose random step move.
 			if (variants == 3) {
 				i = generator.nextInt(variants + 1);
 				switch (i) {
@@ -175,6 +220,7 @@ public class Ghost extends Actor {
 							directions.get(1)));
 				}
 			}
+			//Get direction of current move.
 			direction = last.getDirectionTo(getLocation());
 			gameGrid.refresh();
 		}
